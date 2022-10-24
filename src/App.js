795f232fad9af6b1b464782b7a47de38d1d61e02
Matt H <matthamel321@gmail.com>
2022-10-24 12:27:57 -0700

    Init
    Initial commit, a work in progress

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import bootText from "./boot.json";
 import './App.css';
+import {useState } from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="bootScreen">
+        <Boot/>
+        <LogoStartup />
+      </section>
+      <main>
+        <p>AHG</p>
+        <Logo/>
+      </main>
     </div>
   );
+}
+
+function LogoStartup(){
+  return(
+    <section className='center'>
+      <img className='logo magenta' src="/logo.png" />
+      <img className='logo cyan' src="/logo.png" />
+      <img className='logo white' src="/logo.png" />
+    </section>
+  );
+}
+
+function Logo(){
+  return(
+    <div className="logoBox">
+      <img className='logoMini magenta' src="/logo.png"/>
+      <img className='logoMini cyan' src="/logo.png" />
+      <img className='logoMini white' src="/logo.png" />
+    </div>
+  )
+}
+
+function Boot(){
+  const [bootI, setBootI] = useState(1)
+  const display = bootText.text.slice(0,bootI).map((item)=>{return <p>{item}</p>});
+
+
+  const bootInc = () => {
+    if(bootI >= bootText.text.length){
+      clearInterval(inter);
+    }
+    setBootI(bootI+1);
+  }
+  
+  const inter = setInterval(bootInc, 13);
+  return(
+    <section className='boot'>
+        {display}
+    </section>
+  )
 }
 
 export default App;
